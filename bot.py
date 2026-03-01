4fe016b4f10c33bbbebea697e332669e01a0d2b6
dttn6sqshr-lang <dttn6sqshr@privaterelay.appleid.com>
2026-03-01 11:08:52 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+TOKEN = "PUT_YOUR_TOKEN_HERE"
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -9,11 +9,11 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     await bot.tree.sync()
-    print(f"✅ {bot.user} is online and commands synced")
+    print(f"✅ Logged in as {bot.user}")
 
-async def load_cogs():
+async def load():
     await bot.load_extension("cogs.game")
 
-bot.loop.create_task(load_cogs())
+bot.loop.create_task(load())
 
 bot.run(TOKEN)
